4978ef5bc5a77abf2d863941be50c4b2649416ff
Bright Adewole <brightoadewole@gmail.com>
2023-10-03 16:13:04 -0700

    loops

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -5,4 +5,4 @@ ascii_value_of_a = ord('a')
 for i in range(26):
     current_char = chr(ascii_value_of_a + i)
     if current_char != 'q' and current_char != 'e':
-        print(current_char, end='')
+        print(f"{current_char}", end='')
