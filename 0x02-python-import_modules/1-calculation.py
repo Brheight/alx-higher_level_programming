2b7573a130cbb905bd1968348949aaf280a2652c
Bright Adewole <brightoadewole@gmail.com>
2023-10-05 17:17:44 -0700

    os

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div	
-	
+from calculator_1 import add, sub, mul, div
+
 if __name__ == "__main__":
     a = 10
     b = 5
 
-    result_add = add(a, b)
-    result_sub = sub(a, b)
-    result_mul = mul(a, b)
-    result_div = div(a, b)
+    add_result = add(a, b)
+    sub_result = sub(a, b)
+    mul_result = mul(a, b)
+    div_result = div(a, b)
 
-    print(f"{a} + {b} = {result_add}")
-    print(f"{a} - {b} = {result_sub}")
-    print(f"{a} * {b} = {result_mul}")
-    print(f"{a} / {b} = {result_div}")
+    operations = {
+        "add": add_result,
+        "sub": sub_result,
+        "mul": mul_result,
+        "div": div_result
+    }
+
+    for operation, result in operations.items():
+        print(f"{a} {operation} {b} = {result}")
