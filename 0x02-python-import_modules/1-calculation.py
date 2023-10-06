f2c13577ce4213fab8aaa8e7f61e61925379c728
Bright Adewole <brightoadewole@gmail.com>
2023-10-05 17:20:41 -0700

    os

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-
+a = 10
+b= 5
 if __name__ == "__main__":
-    a = 10
-    b = 5
-
+    
     add_result = add(a, b)
     sub_result = sub(a, b)
     mul_result = mul(a, b)
