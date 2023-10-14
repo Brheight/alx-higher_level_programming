d4cd18f49ed5be576cd44f0aec638cfda3a0eaff
Bright Adewole <brightoadewole@gmail.com>
2023-10-13 17:31:31 -0700

    matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+
+    for row in matrix:
+        new_row = []
+        for element in row:
+            new_row.append(element ** 2)
+        new_matrix.append(new_row)
+
+    return new_matrix
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)
+print(matrix)
