040b39fde3ba3f7e8e3b7df70f914ffa73b622bc
Bright Adewole <brightoadewole@gmail.com>
2023-10-13 17:44:20 -0700

    matrix

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique_set = set()  # Create an empty set to store unique integers
+    result = 0
+    
+    for number in my_list:
+        if number not in unique_set:
+            unique_set.add(number)  
+            result += number 
+    
+    return result
+
+my_list = [1, 2, 3, 1, 4, 2, 5]
+result = uniq_add(my_list)
