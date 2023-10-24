dd526d6c66d02a0b0834b0f94930d8256bdcf019
Bright Adewole <brightoadewole@gmail.com>
2023-10-24 07:33:34 -0700

    class square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    This is the Square class.
+
+    The Square class represents a square shape.
+
+    Args:
+        size (int): The size of the square.
+
+    Attributes:
+        __size (int): The private size of the square.
+
+    """
+    def __init__(self, size):
+        self.__size = size
