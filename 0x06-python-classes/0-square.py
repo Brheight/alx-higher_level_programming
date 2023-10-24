cce093ada225bdd5a7d48deb31ab0d818d7272b4
Bright Adewole <brightoadewole@gmail.com>
2023-10-24 07:27:16 -0700

    readme renamed

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+
 class Square:
+    """
+    This is the Square class.
+
+    The Square class represents a square shape.
+    It doesn't have any attributes or methods yet.
+
+    """
     pass
