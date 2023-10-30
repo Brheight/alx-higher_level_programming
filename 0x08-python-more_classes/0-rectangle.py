9b82f469cf7b4747e6a02c119765a04fe75f2cd5
Bright Adewole <brightoadewole@gmail.com>
2023-10-30 15:22:08 -0700

    rectanglee

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Rectangle"""
+
+
+class Rectangle:
+    """
+    This is the Rectangle class.
+    The Rectangel presents a rectangle shape.
+    It doesn't have any attributes or methods yet
+    """
+    pass
