146012d98be5a848bf945de5108293d23a768f9d
Bright Adewole <brightoadewole@gmail.com>
2023-11-13 14:32:52 -0800

    5-ain

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -82,3 +82,8 @@ class Rectangle(Base):
         """ Display the Rectangle instance using '#' """
         for _ in range(self.height):
             print('#' * self.width)
+
+    def __str__(self):
+        """ Override __str__ method to return a formatted string """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.x, self.y, self.width, self.height)
