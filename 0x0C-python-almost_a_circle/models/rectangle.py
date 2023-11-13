f7bd6a212d1c8d014ff64b498d2a8d6a0201ba5b
Bright Adewole <brightoadewole@gmail.com>
2023-11-13 14:14:26 -0800

    check

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" Module containing the Rectangle class """
+""" Module containing the updated Rectangle class """
 
 from models.base import Base
 
 class Rectangle(Base):
-    """ Rectangle class, inherits from Base """
+    """ Updated Rectangle class, inherits from Base """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Constructor for Rectangle class """
@@ -21,8 +21,13 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        """ Setter for width """
-        self.__width = value
+        """ Setter for width with validation """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
 
     @property
     def height(self):
@@ -31,8 +36,13 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        """ Setter for height """
-        self.__height = value
+        """ Setter for height with validation """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = value
 
     @property
     def x(self):
@@ -41,8 +51,13 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        """ Setter for x """
-        self.__x = value
+        """ Setter for x with validation """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = value
 
     @property
     def y(self):
@@ -51,5 +66,10 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        """ Setter for y """
-        self.__y = value
+        """ Setter for y with validation """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = value
