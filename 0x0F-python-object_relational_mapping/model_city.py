cca461f29a9c5f5a71714a58515b8abd56283f96
Bright Adewole <brightoadewole@gmail.com>
2024-01-15 14:50:43 -0800

    sql py

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module that contains the City class
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base, State
+
+class City(Base):
+    """
+    City class to represent the cities table in MySQL hbtn_0e_14_usa database
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
