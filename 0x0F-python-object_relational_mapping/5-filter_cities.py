b8291d867c71db8f26ca9d06c608b61fcd042451
Bright Adewole <brightoadewole@gmail.com>
2024-01-15 14:20:15 -0800

    sql py

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""
+This script takes in the name of a state as an argument and lists all cities of that state from the database hbtn_0e_4_usa.
+
+It is designed to be safe from SQL injection.
+
+Usage:
+    ./5-filter_cities.py <mysql_username> <mysql_password> <database_name> <state_name>
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+
+    query = """
+            SELECT GROUP_CONCAT(name SEPARATOR ', ')
+            FROM cities
+            JOIN states ON cities.state_id = states.id
+            WHERE states.name = %s
+            ORDER BY cities.id
+            """
+
+    cursor.execute(query, (sys.argv[4],))
+
+    result = cursor.fetchone()
+
+    if result:
+        print(result[0])
+
+    cursor.close()
+    db.close()
