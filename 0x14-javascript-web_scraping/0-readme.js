cece962691ea2c4dc8bded9a2728bc484a7294ce
Bright <brightoadewole@gmail.com>
2024-02-29 03:45:25 -0800

    js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
 const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', (error, data) => {
-  if (error) {
-    console.error(error);
-    return;
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data.trim()); // Trim trailing whitespace
   }
-  
-  console.log(data);
 });
