81fb528155bbfac82a07ab56cd7df0bfe62cd778
Bright <brightoadewole@gmail.com>
2024-02-29 04:19:44 -0800

    js

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+request(apiUrl, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const movieData = JSON.parse(body);
+    const charactersUrls = movieData.characters;
+
+    charactersUrls.forEach((characterUrl) => {
+      request(characterUrl, (charError, charResponse, charBody) => {
+        if (!charError && charResponse.statusCode === 200) {
+          const characterData = JSON.parse(charBody);
+          console.log(characterData.name);
+        } else {
+          console.error(`Error: ${charError}`);
+        }
+      });
+    });
+  } else {
+    console.error(`Error: ${error}`);
+  }
+});
