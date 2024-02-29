7b1059ed92680209df66d6ec8bc8e9269bfb6c1c
Bright <brightoadewole@gmail.com>
2024-02-29 04:13:01 -0800

    js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const filePath = process.argv[3];
+request(url, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    fs.writeFileSync(filePath, body, 'utf-8');
+  } else {
+    console.error(`Error: ${error}`);
+  }
+});
